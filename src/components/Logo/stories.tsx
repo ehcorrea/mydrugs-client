0a038c86a4b791b401fe3ff7983696496307b364
Emilio Correa <ehcorrea60@gmail.com>
2022-02-22 01:12:43 -0300

    feat: 🚀 <Logo/>
    Create a logo component with test and stories

---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,10 @@
+import { Story, Meta } from '@storybook/react';
+import Logo from '.';
+import { LogoProps } from './types';
+
+export default {
+  title: 'Logo',
+  component: Logo,
+} as Meta;
+
+export const Default: Story<LogoProps> = (args) => <Logo {...args} />;
