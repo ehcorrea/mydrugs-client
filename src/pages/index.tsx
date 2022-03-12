04601498f6a0b3cc4c6e0a4905205e86e923400c
Emilio Correa <ehcorrea60@gmail.com>
2022-03-12 18:11:36 -0300

    feat: 🚀 Page Index
    created a page that show <Home/> template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <div />;
+import BANNER_SLIDER_MOCK from 'components/BannerSlider/mock';
+import DRUG_CARD_MOCK from 'components/DrugCardSlider/mock';
+import HIGHLIGHT_MOCK from 'components/Highlight/mock';
+import Home from 'templates/Home';
+import { HomeTemplateProps } from 'templates/Home/types';
+import shuffleArray from 'utils/helpers/shuffleArray';
+
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />;
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      banners: BANNER_SLIDER_MOCK,
+      mostPopularDrugs: shuffleArray(DRUG_CARD_MOCK),
+      mostPopularHighlight: HIGHLIGHT_MOCK,
+      newDrugs: shuffleArray(DRUG_CARD_MOCK),
+      upcommingDrugs: shuffleArray(DRUG_CARD_MOCK),
+      upcommingHighlight: HIGHLIGHT_MOCK,
+      upcommingMoreDrugs: shuffleArray(DRUG_CARD_MOCK),
+    },
+  };
 }
