23e49482a14eb424e4cac481203576340f4395af
Emilio Correa <ehcorrea60@gmail.com>
2022-03-12 20:07:29 -0300

    feat: 🚀 <Highlight/>
    updated imgs and text

--- src/components/Highlight/mock.ts
+++ src/components/Highlight/mock.ts
@@ -1,11 +1,14 @@
-const HIGHLIGHT_MOCK = {
-  title: 'MDMA for good party',
-  subtitle: 'Come see MDMA trips',
-  backgroundImage: '/img/mdma-img.jpg',
+import { HighlightProps } from './types';
+
+const HIGHLIGHT_MOCK: HighlightProps = {
+  title: 'Your trippy',
+  subtitle: 'your choice',
+  backgroundImage: '/img/highlight.jpg',
   button: {
     label: 'Buy now',
     link: '/mdma',
   },
+  floatImage: '/img/trippy.png',
 };
 
 export default HIGHLIGHT_MOCK;
