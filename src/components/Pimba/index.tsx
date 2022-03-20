ee71b772af19afab7e89dc20956689c434a65bfc
Emilio Correa <ehcorrea60@gmail.com>
2022-03-20 19:24:34 -0300

    fix: 🐛 Component
    remove non used component

--- src/components/Pimba/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as S from './styles';
-
-const Pimba = () => <S.Wrapper>Pimba</S.Wrapper>;
-
-export default Pimba;
--- src/components/Pimba/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react';
-import Pimba from '.';
-
-export default {
-  title: 'Pimba',
-  component: Pimba,
-} as Meta;
-
-export const Default: Story = () => <Pimba />;
