2ba5930e82c5eda509e054970c29dd8fb72fe757
Emilio Correa <ehcorrea60@gmail.com>
2022-04-05 16:21:49 -0300

    refactor: 💡 <Button/>
    added fowardRef to component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,22 @@
-import { useMemo } from 'react';
+import { useMemo, forwardRef } from 'react';
 import * as S from './styles';
-import { ButtonProps } from './types';
+import { ButtonProps, WrapperProps } from './types';
 
-const Button = ({ children, size = 'medium', icon, ...props }: ButtonProps) => {
+const Button: React.ForwardRefRenderFunction<WrapperProps, ButtonProps> = (
+  { children, size = 'medium', icon, ...props },
+  ref
+) => {
   const renderChildren = useMemo(
     () => !!children && <span>{children}</span>,
     [children]
   );
 
   return (
-    <S.Wrapper size={size} hasIcon={!!icon} {...props}>
+    <S.Wrapper size={size} hasIcon={!!icon} ref={ref} {...props}>
       {icon}
       {renderChildren}
     </S.Wrapper>
   );
 };
 
-export default Button;
+export default forwardRef(Button);
