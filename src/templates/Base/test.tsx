4362f13b74cb1408dfb6000facba2f7adf52ebbd
Emilio Correa <ehcorrea60@gmail.com>
2022-04-07 13:42:11 -0300

    feat: 🚀 template <Base/>
    created a template with tests

---
+++ src/templates/Base/test.tsx
@@ -0,0 +1,36 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import Base from '.';
+
+jest.mock('components/Menu', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Menu"></div>;
+    },
+  };
+});
+
+jest.mock('components/Footer', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Footer"></div>;
+    },
+  };
+});
+
+describe('<Base />', () => {
+  it('should render the Base with menu, children e footer', () => {
+    renderWithTheme(
+      <Base>
+        <p>children element</p>
+      </Base>
+    );
+
+    expect(screen.getByTestId(/mock menu/i)).toBeInTheDocument();
+    expect(screen.getByText(/children element/i)).toBeInTheDocument();
+    expect(screen.getByTestId(/mock footer/i)).toBeInTheDocument();
+  });
+});
